3ec4df68927f3eda05f4738c28d0c7e595795ba7
Will Lawrence <will@aha.io>
2022-06-27 15:56:56 -0400

    Add request

--- src/views/samplePage.js
+++ src/views/samplePage.js
@@ -15,11 +15,33 @@ const Styles = () => {
   );
 };
 
+function getRecentWorkflowData() {
+  const url =
+    "https://circleci.com/api/v2/insights/gh/aha-app/aha-app/workflows?branch=master&reporting-window=last-7-days";
+  const settings = {
+    method: "GET",
+    headers: {
+      "Circle-Token": "",
+      Host: "circleci.com",
+      "content-type": "application/json",
+      Accept: "application/json",
+    },
+    mode: "no-cors",
+  };
+
+  fetch(url, settings)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
+
 aha.on("samplePage", ({ fields, onUnmounted }, { identifier, settings }) => {
   return (
     <>
       <Styles />
-      <div className='title'>Sample Page</div>
+      <div className="title">Sample Page. You made it!</div>
+      <div>{getRecentWorkflowData()}</div>
     </>
   );
 });
