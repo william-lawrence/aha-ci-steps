7e2bd90cfa06544063f1b3350fa24db9b45da72c
Will Lawrence <will@aha.io>
2022-07-22 16:42:53 -0400

    Render success rates

--- src/views/samplePage.js
+++ src/views/samplePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Styles = () => {
   return (
@@ -15,33 +15,68 @@ const Styles = () => {
   );
 };
 
-function getRecentWorkflowData() {
-  const url =
-    "https://circleci.com/api/v2/insights/gh/aha-app/aha-app/workflows?branch=master&reporting-window=last-7-days";
-  const settings = {
+aha.on("samplePage", ({ fields, onUnmounted }, { identifier, settings }) => {
+  return <Foo></Foo>;
+});
+
+function getRecentWorkflowData(daysSinceUpdated, callback) {
+  let date = new Date();
+  date.setDate(date.getDate() - daysSinceUpdated); // We want to get all the workflows on master for the last N days
+
+  const url = `https://big.aha.io/api/v1/products/ENG/custom_objects/circleci_workflows/records`;
+
+  fetch(url, {
     method: "GET",
     headers: {
-      "Circle-Token": "",
-      Host: "circleci.com",
-      "content-type": "application/json",
+      "Content-Type": "application/json",
       Accept: "application/json",
+      Authorization: "Bearer ",
     },
-    mode: "no-cors",
-  };
-
-  fetch(url, settings)
+  })
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      callback(json);
     });
 }
 
-aha.on("samplePage", ({ fields, onUnmounted }, { identifier, settings }) => {
+function calculateSuccessRate(workflows, daysSinceUpdated) {
+  let successCount = 0;
+  let totalWorkflows = workflows.custom_object_records.length;
+  workflows.custom_object_records.forEach((workflow) => {
+    const statusWorkflows = workflow.custom_fields.filter(
+      (field) => field.name === "workflow_status" && field.value === "success"
+    );
+
+    successCount += statusWorkflows.length;
+  });
+
+  return (successCount / totalWorkflows) * 100;
+}
+
+function Foo(props) {
+  const [workflows, setWorkflows] = useState(null);
+
+  getRecentWorkflowData(1, setWorkflows);
+
+  let oneDaySuccessRate;
+  let sevenDaySuccessRate;
+
+  if (workflows) {
+    oneDaySuccessRate = calculateSuccessRate(workflows, 1);
+    sevenDaySuccessRate = calculateSuccessRate(workflows, 7);
+  }
+
   return (
     <>
       <Styles />
-      <div className="title">Sample Page. You made it!</div>
+      <div className="title">
+        Success rate for the last 24 Hours: {oneDaySuccessRate}%
+      </div>
+      <div className="title">
+        Success rate for the last 7 Days: {sevenDaySuccessRate}%
+      </div>
       <div>{getRecentWorkflowData()}</div>
     </>
   );
-});
+}
